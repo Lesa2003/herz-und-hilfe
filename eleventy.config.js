d489cea59479d4f25288efadb502cba713d9acfa
Ahmad Alshehab <ahmad.alshehab@outlook.de>
2026-08-31 22:22:40 +0200

    Configure site for custom domain herzundhilfe.com (root path, CNAME file)

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ const markdownIt = require("markdown-it")();
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addGlobalData("layout", "base.njk");
   eleventyConfig.addFilter("markdownInline", (content) =>
@@ -18,6 +19,6 @@ module.exports = function (eleventyConfig) {
       output: "_site",
       includes: "_includes",
     },
-    pathPrefix: "/herz-und-hilfe/",
+    pathPrefix: "/",
   };
 };
